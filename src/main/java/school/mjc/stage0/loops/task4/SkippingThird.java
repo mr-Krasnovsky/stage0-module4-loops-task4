fa8f3938667d6be9d8ee1b850dafe3c8f1bb4f5a
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-08-01 13:42:52 +0300

    Complited

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        int count = 1;
+        for (int i = 1; i <= lastPrinted; i++) {
+            if (count == 3) {
+                count = 1;
+                continue;
+            }
+            count++;
+            System.out.println(i);
+        }
     }
 }
